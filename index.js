00267fbef54ec189e762e0ddaa86156e5532de23
Shane McDermott <shanemcdermott@unomaha.edu>
2017-10-29 22:31:47 +0000

    Attempted to replicate key presses

--- index.js
+++ index.js
@@ -11,81 +11,100 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+//TODO- Ask about improving this!
+app.get('/city.png', function(req,res){
+   res.sendFile(__dirname + '/city.png'); 
+});
+
 var waitingForGame = [];
 var games = [];
 
 function gameInstance(id, socket1, socket2, objects)
 {
     this.id = id;
-    this.players = [socket1, socket2];
+    this.sockets = [socket1, socket2];
     this.objects = objects;
-}
-
-function vec2(x, y)
-{
-    this.x = x;
-    this.y = y;
-    this.add = function(v2)
-    {
-        this.x += v2.x;
-        this.y += v2.y;
-    }
-    this.sub = function(v2)
-    {
-        this.x -= v2.x;
-        this.y -= v2.y;
+    this.state = "playing";
+    this.players = [];
+          
+    this.addObject = function(gameObject) {
+        let obj_id = this.objects.length;
+        this.objects.push(gameObject);
+        return obj_id;
     }
 }
 
-function gameObject(id, width, height, x, y) {
+
+
+function component(id, name, width, height, src, x, y, type) {
+    this.type = type;
     this.id = id;
+    this.name = name;
+    this.src = src;
     this.width = width;
     this.height = height;
-    this.loc = new vec2(x,y);
-    this.vel = new vec2(0,0);
-    this.color = "black";
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
     
-    this.update = function(){
-        this.loc.x += this.vel.x;
-        this.loc.y += this.vel.y;
+    this.setSpeed = function(x, y) {
+        this.speedX = x;
+        this.speedY = y;
     }
     
-    this.render = function(ctx){
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.loc.x, this.loc.y, this.width, this.height);
+    this.update = function() {
+      this.updatePos();
     }
-
-    this.move = function(dir){
-        this.vel = dir;
-    }
-    this.stopMove = function()
+    
+    this.updatePos = function()
     {
-        this.vel = new vec2(0,0);
+        this.x += this.speedX;
+        this.y += this.speedY;          
     }
 }
 
+function controller(pawn_id, team)
+{
+    this.pawn_id = pawn_id;
+    this.team = team;
+    this.keys = [];
+    
+    /*
+    this.updatePawn = function()
+    {
+        let dirX = 0;
+        let dirY = 0;
+        if(this.keys)
+        {
+            if(this.keys[37]){dirX = -1;}
+            if(this.keys[39]){dirX = 1;}
+            if(this.keys[38]){dirY = -1;}
+            if(this.keys[40]){dirY = 1;}
+        }
+        game.objects[this.pawn_id].setSpeed(dirX,dirY);
+    }
+    */
+}
 
-
+//currently just shares the keypress with the other player
 io.on('connection', function(socket){
 
-    socket.on('move', function(msg){
-        
+    socket.on('client_keypress', function(msg){
         if(msg.gameId>=games.length){return;}
-
         var game = games[msg.gameId];
         console.log(msg);
-
-        for(let i = 0; i < msg.objects.length; i++)
+        for(let i = 0; i < game.sockets.length; i++)
         {
-            game.objects[msg.objects[i].id].loc = msg.objects[i].loc;
+            if(game.sockets[i]!= socket)
+            {
+                
+              game.sockets[i].emit("server_keypress", msg);
+            }
         }
-        for(let i = 0; i < game.players.length; i++)
-        {
-            game.players[i].emit('move', msg.objects);
-        }
-
-        games[msg.gameId] = game;
+        
     });
+
     
     socket.on('requestGame', function(msg){
         
@@ -104,20 +123,25 @@ io.on('connection', function(socket){
             
             var game = new gameInstance(games.length, socket1, socket2, []);
 
-            game.objects.push(new gameObject(0, 30,30,10,120));
-            game.objects.push(new gameObject(1,30,30,60,150));
+            game.objects.push(new component(0, "player1", 32,32, "city.png", 10,0,"image"));
+            game.objects.push(new component(1, "player2", 32,32, "blue", 10,0,"block"));
+            game.players.push(new controller(0, 0));
+            game.players.push(new controller(1, 1));
 
             games.push(game);
-
+            
+            
             socket1.emit("Start Game", {
                 "id" : game.id,
                 "playerAssignment" : 0,
+                "players": game.players,
                 "objects": game.objects,
             });
 
             socket2.emit("Start Game", {
                 "id" : game.id,
                 "playerAssignment" : 1,
+                "players": game.players,
                 "objects": game.objects,
             });
         }
