e0c3e69c7ba2b0411a6ef90357d358602c39a3d9
Shane McDermott <shanemcdermott@unomaha.edu>
2017-10-30 19:31:01 +0000

    Cleaned up code

--- index.js
+++ index.js
@@ -18,95 +18,19 @@ app.get('/city.png', function(req,res){
 
 var waitingForGame = [];
 var games = [];
-
-function gameInstance(id, sockets, objects)
-{
-    this.id = id;
-    this.sockets = sockets;
-    this.objects = objects;
-    this.state = "playing";
-    this.players = [];
-          
-    this.addObject = function(gameObject) {
-        let obj_id = this.objects.length;
-        this.objects.push(gameObject);
-        return obj_id;
-    }
-}
+var sessions = [];
 
 
-
-function component(id, name, width, height, src, x, y, type) {
-    this.type = type;
-    this.id = id;
-    this.name = name;
-    this.src = src;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;    
-    
-    this.setSpeed = function(x, y) {
-        this.speedX = x;
-        this.speedY = y;
-    }
-    
-    this.update = function() {
-      this.updatePos();
-    }
-    
-    this.updatePos = function()
-    {
-        this.x += this.speedX;
-        this.y += this.speedY;          
-    }
-}
-
-function controller(pawn_id, team)
-{
-    this.pawn_id = pawn_id;
-    this.team = team;
-    this.keys = [];
-    
-    /*
-    this.updatePawn = function()
-    {
-        let dirX = 0;
-        let dirY = 0;
-        if(this.keys)
-        {
-            if(this.keys[37]){dirX = -1;}
-            if(this.keys[39]){dirX = 1;}
-            if(this.keys[38]){dirY = -1;}
-            if(this.keys[40]){dirY = 1;}
-        }
-        game.objects[this.pawn_id].setSpeed(dirX,dirY);
-    }
-    */
-}
 
 //currently just shares the keypress with the other player
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
 
 //look into debouncing
 
     socket.on('client_keypress', function(msg){
         if(msg.gameId>=games.length){return;}
         socket.broadcast.emit("server_keypress", msg);
-        /*
-        var game = games[msg.gameId];
-        console.log(msg);
-        for(let i = 0; i < game.sockets.length; i++)
-        {
-            if(game.sockets[i]!= socket)
-            {
-                
-              game.sockets[i].emit("server_keypress", msg);
-            }
-        }
-        */
     });
 
     
@@ -129,24 +53,64 @@ io.on('connection', function(socket){
 
 function launchGame(sockets)
 {
-    var game = new gameInstance(games.length, sockets, []);
+    var gameId = games.length;
+    var gameInstance = new game(gameId, []);
+    var session = new gameSession(gameId, sockets);
 
     for(let i = 0; i < sockets.length; i++)
     {
         let name = "player"+i;
-        game.objects.push(new component(i,name,32,32,"blue",10,i*10,"block"));
-        game.players.push(new controller(i,i));
+        gameInstance.objects.push(new component(i,name,32,32,"blue",10,i*10,"block"));
+        gameInstance.players.push(new controller(i,i));
     }
  
-    games.push(game);
-        
+    games.push(gameInstance);
+    sessions.push(session);
+    
     for(let i = 0; i < sockets.length; i++)
     {
         sockets[i].emit("Start Game", {
-            "id" : game.id,
+            "game" : gameInstance,
             "playerAssignment" : i,
-            "players": game.players,
-            "objects": game.objects,
         });
     }
+}
+
+
+//Object Types
+
+function gameSession(id, sockets)
+{
+    this.id = id;
+    this.sockets = sockets;
+}
+
+
+function game(id, objects)
+{
+    this.id = id;
+    this.players = [];
+    this.objects = objects;
+    this.state = "playing";
+}
+
+
+function component(id, name, width, height, src, x, y, type) {
+    this.type = type;
+    this.id = id;
+    this.name = name;
+    this.src = src;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+}
+
+function controller(id, pawn_id)
+{
+    this.id = id;
+    this.pawn_id = pawn_id;
+    this.keys = [];
 }
