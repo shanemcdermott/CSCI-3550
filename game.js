d5b27ef3bc6b90a0875757cbc9bfe3a7b18d90c7
Shane McDermott <shanemcdermott@unomaha.edu>
2017-11-19 02:34:28 +0000

    Changed to Unreal based project

--- game.js
+++ game.js
@@ -1,79 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var util = require('util');
+var clients = [];
 
-function gameInstance(id, players)
-{
-  this.id = id;
-  this.players = players;
-  this.state = new startState(id,players);
-  this.interval = setInterval(updateGame, 20);
+io.on ('connection', function(socket){
+    clients.push(socket.id);
+    var clientConnectedMsg = 'User connected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+    console.log(clientConnectedMsg);
+
+    socket.on('disconnect', function(){
+        clients.pop(socket.id);
+        var clientDisconnectedMsg = 'User disconnected ' + util.inspect(socket.id) + ', total: ' + clients.length;
+        console.log(clientDisconnectedMsg);
+    })
+});
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
+
+function getRandomInRange(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-function gameState(id, objects)
+function sendWind()
 {
-    this.id=id;
-    this.objects=objects;
-    
-    this.update = function()
-    {
-        for(let i = 0; i < this.objects.length; i++)
-        {
-            this.objects[i].update();
-        }
-    }
+    console.log('Wind sent to user');
+    io.emit('new wind', getRandomInRange(0,360));
 }
 
-function startState(id)
-{
-    var objects = [
-        new gameObject(0,32,32,16,48,"blue","shape"),
-        new gameObject(0,32,32,64,64,"red","shape"),
-        ];
-    
-    return new gameState(id,objects);
-}
-
-function updateGame()
-{
-    
-}
-
-function gameObject(id, width, height, x, y, color, type)
-{
-    this.id = id;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.type=type;
-    if(type == "image")
-    {
-        this.image = new Image();
-        this.image.src= color;
-    }
-    else
-    {
-        this.color=color;
-    }
-    this.speedX=0;
-    this.speedY=0;
-    
-    this.update = function()
-    {
-        this.x += speedX;
-        this.y += speedY;
-    }
-    
-    this.render = function()
-    {
-        ctx = gameArea.context;
-        if(type == "image")
-        {
-            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        }
-        else
-        {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-}
-
+setInterval(sendWind, 3000);
